43f6078bc6b082b4573f0b33845126cea35a97a9
Allie Soldau <alliesoldau@gmail.com>
2024-03-05 10:04:13 -0500

    Switched education formatting to be cards

--- src/components/Edu.js
+++ src/components/Edu.js
@@ -2,21 +2,19 @@ import { Grid, Hidden } from "@mui/material";
 
 function Edu({ eduDetails }) {
   return (
-    <Grid container className="edu-block" spacing={2}>
-      <Hidden mdDown>
-        {eduDetails.logo ? (
-          <div class="logo-container">
-            <img
-              src={process.env.PUBLIC_URL + `/Logos/${eduDetails.logo}`}
-              alt={`${eduDetails.employer} logo`}
-            />
-          </div>
-        ) : null}
-      </Hidden>
+    <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className="edu-contents">
+      {/* <Hidden mdDown> */}
+      {eduDetails.logo ? (
+        <div class="logo-container">
+          <img
+            src={process.env.PUBLIC_URL + `/Logos/${eduDetails.logo}`}
+            alt={`${eduDetails.employer} logo`}
+          />
+        </div>
+      ) : null}
+      {/* </Hidden> */}
       <div className="edu">
         <div className="edu-header">
-          <p>{eduDetails.program}</p>
-          <p className="divider">⸻</p>
           <a
             className="institution"
             href={`${eduDetails.url}`}
@@ -26,11 +24,12 @@ function Edu({ eduDetails }) {
           >
             <p className="institution">{eduDetails.institution}</p>
           </a>
+          <p className="program">{eduDetails.program}</p>
+          <p className="edu-blurb subtle">{eduDetails.blurb}</p>
+          <p className="years subtle">
+            {eduDetails.startYear} - {eduDetails.endYear}
+          </p>
         </div>
-        <p className="edu-blurb subtle">{eduDetails.blurb}</p>
-        <p className="years subtle">
-          {eduDetails.startYear} - {eduDetails.endYear}
-        </p>
       </div>
     </Grid>
   );
